48f93418d275420ac685a1364a9efd5cc39aa7e0
epraschifka <edwardpraschifka@gmail.com>
2026-05-20 17:47:41 +0930

    Add: Network class
    Define network class in network.py.
    Add tests in tests/test_network.py

---
+++ src/network.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Network:
+    def __init__(self, layers: np.ndarray):
+        self.layers = np.array(int)
+        self.size = len(layers)
+        self.weights = [0] * self.size
+        self.biases = [0] * self.size
+
+        for i in range(self.size-1):
+            self.weights[i] = np.random.rand(layers[i], layers[i+1])
+        
+        for i in range(self.size):
+            self.biases[i] = np.random.rand(layers[i])
---
+++ tests/test_network.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+
+from src.network import Network
+
+def test_network_size():
+    """Create a 3-layer network with layers of size 3,4,2"""
+
+    layers = np.array([3,4,2])
+    nw = Network(layers)
+
+    assert nw.size == 3
+
+    assert len(nw.weights) == 3
+    assert nw.weights[0].shape == (3,4)
+    assert nw.weights[1].shape == (4,2)
+    
+    assert len(nw.biases) == 3
+    assert len(nw.biases[0]) == 3
+    assert len(nw.biases[1]) == 4
+    assert len(nw.biases[2]) == 2
+    
