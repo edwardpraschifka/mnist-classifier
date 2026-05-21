934be1fec19cf8c1878c3b8aad302ffe77b22e4d
epraschifka <edwardpraschifka@gmail.com>
2026-05-21 13:49:23 +0930

    Add: feedforward method to network class

--- src/network.py
+++ src/network.py
@@ -1,13 +1,49 @@
 import numpy as np
 
+from .utils import cost, sigmoid
+
 class Network:
     def __init__(self, layers: np.ndarray):
+        """Creates a new network, where layer i has layers[i] nodes"""
+
         if type(layers) != np.ndarray: 
             raise TypeError("layers expected <np.ndarray>" 
                         f", got {type(layers)}")
     
         self.layers = layers
         self.size = len(layers)
-
+        
         self.weights = [np.random.rand(layers[i+1], layers[i]) for i in range(self.size - 1)]
         self.biases = [np.random.rand(layers[i]) for i in range(1, self.size)]
+
+    def feedforward(self, X: np.ndarray):
+        """Feeds X into network, returns corresponding Y"""
+
+        if type(X) != np.ndarray: 
+            raise TypeError("X expected <np.ndarray>" 
+                        f", got {type(X)}")
+        
+        # check that the shape of X matches
+        # the number of nodes in the input layer
+        if np.shape(X) != (self.layers[0],):
+            raise ValueError("Expected X to have shape"
+                             f" ({self.layers[0]}, ), but "
+                             f"got shape {np.shape(X)}")
+        
+        current_layer = X
+
+        for i in range(self.size - 1):
+            # multiply input vector by weight matrix
+            current_layer = self.weights[i] @ current_layer
+
+            # add bias vector to weighted sum
+            current_layer += self.biases[i]
+
+            # call activation function
+            current_layer = sigmoid(current_layer)
+        
+        return current_layer
+
+
+
+        
--- tests/test_network.py
+++ tests/test_network.py
@@ -26,3 +26,38 @@ class TestConstructor:
         with pytest.raises(TypeError):
             layers = list([])
             nw = Network(layers)
+
+
+class TestFeedForward:
+
+    def test_bad_type(self):
+        layers = np.array([3,4,2])
+        nw = Network(layers)
+        X = [1,2,3]
+
+        with pytest.raises(TypeError):
+            nw.feedforward(X)
+
+    def test_diff_len(self):
+        layers = np.array([3,4,2])
+        nw = Network(layers)
+        X = np.array([1,2,3,4])
+
+        with pytest.raises(ValueError):
+            nw.feedforward(X)
+    
+    def test_output(self):
+        layers = np.array([3,4,2])
+        nw = Network(layers)
+        X = np.array([0.1, 0.1, 0.2]).reshape((3,))
+
+        nw.weights[0] = np.array([[0.1, 0.2, 0.3], [0.5, 0.4, 0.2], 
+                               [0.1, 0.1, 0.2], [0.3, 0.2 , 0.1]])
+        nw.weights[1] = np.array([[0.2, 0.1, 0.2, 0.2], [0.3, 0.1, 0.3, 0.1]])
+
+        
+        nw.biases[0] = np.array([0.1, 0.2, -0.1, 0.3])
+        nw.biases[1] = np.array([0.1, 0.2])
+
+        out = nw.feedforward(X)
+        assert np.array_equal(np.round(out,4), np.array([0.6187, 0.6522]))
