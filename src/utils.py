d6fed84cc97eab2bada50b4a0ee638bead5e06e3
epraschifka <edwardpraschifka@gmail.com>
2026-05-26 13:56:12 +0930

    Change: utils.cost requires strict input shape
    Forces vectors to be of shape (n,1) at all times

--- src/utils.py
+++ src/utils.py
@@ -2,19 +2,29 @@ import numpy as np
 import math
 
 def cost(y_actual: np.ndarray, y: np.ndarray):
+    """Computes the difference between two (n,1) vectors"""
+
     if type(y_actual) != np.ndarray: 
-        raise TypeError("y_actual expected <np.ndarray>" 
+        raise TypeError("y_actual expected <np.ndarray>"
                         f", got {type(y_actual)}")
     
     if type(y) != np.ndarray: 
         raise TypeError("y expected <np.ndarray>" 
                         f", got {type(y)}")
     
-    if len(y_actual) != len(y):
-        raise ValueError("Size mismatch: y_actual has size "
-                         f"{len(y_actual)}, while "
-                         f"y has size {len(y)}")
+    if np.shape(y_actual) != (len(y_actual),1): 
+        raise ValueError("y_actual: expected shape "
+                         f"({len(y_actual)}, 1), "
+                         f"got {np.shape(y_actual)}")
     
+    if np.shape(y) != (len(y),1): 
+        raise ValueError(f"y: expected shape ({len(y)},1), " 
+                         "got {np.shape(y)}")
+    
+    if np.shape(y_actual) != np.shape(y):
+        raise ValueError("Shape mismatch: y_actual has "
+                         f"shape {np.shape(y_actual)}, "
+                         f"but y has shape {np.shape(y)}")
     
         
     diff_sq = (y_actual - y)**2
